adc7f7517332bb5c6fd219f4dc7938edfcdf5cbf
Maranata <maranataflk9@gmail.com>
2022-07-11 01:57:02 -0700

    simple

---
+++ README.md
@@ -0,0 +1,1 @@
+This directory includes the project simple shell.
---
+++ simple_code.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int c;
+	c = 2;
+	printf("%d \n",c);
+}
